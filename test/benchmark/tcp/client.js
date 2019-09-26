3cd5070809e59dcdfc18754f958b752c9ab36455
amiiigh <ghafari.ah@gmail.com>
2019-09-26 16:04:39 -0400

    rudp server test

--- test/benchmark/tcp/client.js
+++ test/benchmark/tcp/client.js
@@ -6,19 +6,29 @@ var args = process.argv.slice(2);
 var filePath = args[2]
 var serverPort = args[1]
 var serverAddress = args[0]
-var readStream = fs.createReadStream(filePath);
-var client = new net.Socket();
+var readStream = fs.createReadStream(filePath)
+var client = new net.Socket()
+var timerIsRunning = false
+var startTime = 0
+var endTime = 0
 client.connect(serverPort, serverAddress);
 readStream.on('error', function(e) {
 	console.error(e);
 });
 readStream.on('data', function(chunk) {
+	if (!timerIsRunning) {
+		timerIsRunning = true
+		startTime = process.hrtime();
+	}
 	client.write(chunk)
 });
+
 readStream.on('end', function() {
+	var endTime = process.hrtime(startTime);
+	console.log(chalk.bold.green('File has been sent', endTime[1]/1000000, ' ms'))
 	client.end();
 });
 
 client.on('end', () => {
-  chalk.bold.yellow('disconnected from server');
+  console.log(chalk.bold.yellow('Disconnected from server'))
 });
