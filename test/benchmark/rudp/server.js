e1e4c6e1fd3d95239d09421b532a898611d4178f
amiiigh <ghafari.ah@gmail.com>
2019-09-26 16:09:54 -0400

    fix lib address file test

--- test/benchmark/rudp/server.js
+++ test/benchmark/rudp/server.js
@@ -1,15 +1,18 @@
 var rudp = require('../../../lib');
 var dgram = require('dgram');
 var fs = require('fs');
+var args = process.argv.slice(2);
 var serverPort = args[0]
 
 var dest = fs.createWriteStream('received.json');
 var serverSocket = dgram.createSocket('udp4');
 serverSocket.bind(serverPort);
+console.log('server is running on ', serverPort)
 var server = new rudp.Server(serverSocket);
 
 server.on('connection', (connection) => {
-  connection.on('data', (data) => {
+	console.log('new connection')
+	connection.on('data', (data) => {
   	console.log(data.toString())
   	dest.write(data)
   });
