d8626e28446b62e410574a4b36528f26c1eb8b3d
amiiigh <ghafari.ah@gmail.com>
2019-09-29 22:10:17 -0400

    change file name to received rudp

--- test/benchmark/rudp/server.js
+++ test/benchmark/rudp/server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var args = process.argv.slice(2);
 var serverPort = args[0]
 
-var dest = fs.createWriteStream('received.json');
+var dest = fs.createWriteStream('received');
 var serverSocket = dgram.createSocket('udp4');
 serverSocket.bind(serverPort);
 console.log('server is running on ', serverPort)
@@ -18,6 +18,7 @@ serverSocket.on('message', function (message, rinfo) {
 	var addressKey = rinfo.address + rinfo.port;
 	var connection;
 	if (!_connections[addressKey]) {
+		console.log('new connection', addressKey)
 	  connection = new rudp.Connection(new rudp.PacketSender(serverSocket, rinfo.address, rinfo.port));
 	  connection.on('data', data => {
 		if (!timerIsRunning) {
@@ -25,6 +26,7 @@ serverSocket.on('message', function (message, rinfo) {
 				startTime = process.hrtime();
 			}
 	  	totalSize += data.length
+	  	console.log(totalSize,'/',data.length)
 	  	if (totalSize === fileSize) {
 	  		endTime = process.hrtime(startTime);
 			console.log('File',totalSize,  'has been received', endTime[1]/1000000, ' ms')
